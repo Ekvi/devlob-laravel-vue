70cecae3b9e8488259ecd5f3e5f6254c156cef5c
Dmitrii <dima_sh84@mail.ru>
2017-04-01 23:03:48 +0300

    set up cors

---
+++ front/src/components/authentication/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="form-group">
+                        <input type="email" class="form-control" v-model="email" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" v-model="password" placeholder="Password">
+                    </div>
+                    <button @click="test" type="submit" class="btn btn-success pull-right">Login</button>
+                </div>
+            </div>
+        </div>
+        <pre>
+            {{ $data }}
+        </pre>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            test () {
+                //this.$http.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+                this.$http.get("http://127.0.0.1:8000/api/test")
+                        .then(function(response) {
+                            console.log(response);
+                        })
+            }
+        }
+    }
+
+</script>
+
+<style></style>
---
+++ server/routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+header('Access-Control-Allow-Origin: *');
+header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test', function() {
+    return response()->json([
+        'user' => [
+            "first_name" => 'dmitrii',
+            "last_name" => 'shribak',
+        ]
+    ]);
+    //->header('Access-Control-Allow-Origin', '*');
+});
+
